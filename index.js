f73f7eb0902799a374c70309f90010d82e38fa37
SaltyPleb <sergun4ik22@gmail.com>
2021-09-26 20:01:39 +0300

    create db

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json())
+app.use('/api', router)
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'WORKING'})
+// })
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start()
---
+++ routes/userRouter.js
@@ -0,0 +1,14 @@
+const Router = require('express')
+const router = new Router()
+
+router.post('/registration',)
+router.get('/login',)
+router.get('/auth',)
+
+
+// router.get('/auth', (req, res) => {
+//     res.json({message:'all works'})
+// })
+
+
+module.exports = router
