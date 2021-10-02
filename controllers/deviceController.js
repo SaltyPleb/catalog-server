f36bf3229f1853894a36ab14f7a67bace7b7967b
SaltyPleb <sergun4ik22@gmail.com>
2021-10-02 19:28:32 +0300

    addibg a file via postman and half of te deviceController

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,7 +1,23 @@
+const uuid = require('uuid'); // give us random numbers
+const path = require('path');
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
+
 class deviceController{
-    async create(req, res) {
-        const {name, price, brandId, typeId, info} = res.body
-        const {img} = req.files
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const device = await Device.create({name, price, brandId, typeId, img: fileName})
+
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+        
     }
 
     async getAll(req, res) {
--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler =require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
