5395f1cefa40232d62792f22bd39061c75cf2a26
SaltyPleb <sergun4ik22@gmail.com>
2021-10-02 19:09:52 +0300

    registration fileUpload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const sequelize = require("./db");
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler =require('./middleware/ErrorHandlingMiddleware')
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 
