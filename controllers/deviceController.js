24f43c6b63f4a0ca199e9dc6b23c24ecae6f3f02
SaltyPleb <sergun4ik22@gmail.com>
2021-10-02 20:00:03 +0300

    creating device controller

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,33 +1,58 @@
-const uuid = require('uuid'); // give us random numbers
-const path = require('path');
-const {Device} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const uuid = require("uuid"); // give us random numbers
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../error/ApiError");
 
-class deviceController{
-    async create(req, res, next) {
-        try {
-            const {name, price, brandId, typeId, info} = req.body
-            const {img} = req.files
-            let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+class deviceController {
+  async create(req, res, next) {
+    try {
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuid.v4() + ".jpg";
+      img.mv(path.resolve(__dirname, "..", "static", fileName));
 
-            const device = await Device.create({name, price, brandId, typeId, img: fileName})
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        img: fileName,
+      });
 
-            return res.json(device)
-        } catch (e) {
-            next(ApiError.badRequest(e.message))
-        }
-        
+      return res.json(device);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async getAll(req, res) {
+    let { brandId, typeId , limit, page } = req.query;
+    
+    page = page || 1;
+    limit = limit || 15;
+    let offset = page * limit - limit
+
+    let devices;
+    if (!brandId && !typeId) {
+        devices = await Device.findAndCountAll({limit, offset})
     }
 
-    async getAll(req, res) {
-        
+    if (brandId && !typeId) {
+        devices = await Device.findAndCountAll({where: {brandId}, limit, offset})
     }
 
-    async getOne(req, res) {
-
+    if (!brandId && typeId) {
+        devices = await Device.findAndCountAll({where: {typeId}, limit, offset})
     }
 
+    if(brandId && typeId) {
+        devices = await Device.findAndCountAll({where: {typeId, brandId}, limit, offset})
+    }
+
+    return res.json(devices);
+  }
+
+  async getOne(req, res) {}
 }
 
-module.exports = new deviceController()
+module.exports = new deviceController();
