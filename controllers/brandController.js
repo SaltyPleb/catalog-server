1b360fae46cb31682c2905874810c4e736b32eb1
SaltyPleb <sergun4ik22@gmail.com>
2021-11-06 21:59:21 +0300

    add dep column

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,18 +1,21 @@
-const {Brand} = require('../models/models')
-const ApiError = require('../error/ApiError')
+const { Brand } = require("../models/models");
+const ApiError = require("../error/ApiError");
 
-class brandController{
-    async create(req, res) {
-        const {name} = req.body
-        const brand = await Brand.create({name})
-        return res.json(brand)
+class brandController {
+  async create(req, res, next) {
+    try {
+      const { name, dep } = req.body;
+      const brand = await Brand.create({ name, dep });
+      return res.json(brand);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
     }
+  }
 
-    async getAll(req, res) {
-        const brands = await Brand.findAll()
-        return res.json(brands)
-    }
-
+  async getAll(req, res) {
+    const brands = await Brand.findAll();
+    return res.json(brands);
+  }
 }
 
-module.exports = new brandController()
+module.exports = new brandController();
