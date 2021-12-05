9d1c8d53b9f3bc57af5ff8b1a4e1fd54fdffd59a
SaltyPleb <sergun4ik22@gmail.com>
2021-12-05 18:33:45 +0300

    update role

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,10 @@ const jwt = require("jsonwebtoken");
 const { User, Basket } = require("../models/models");
 
 const generateJwt = (id, email, role) => {
-    return jwt.sign(
-        { id, email, role },
-        process.env.SECRET_KEY,
-        { expiresIn: '24h' }
-      );
-}
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class userController {
   async registration(req, res, next) {
@@ -24,31 +22,29 @@ class userController {
     const hashPassword = await bcrypt.hash(password, 5);
     const user = await User.create({ email, role, password: hashPassword });
     const basket = await Basket.create({ userId: user.id });
-    const token = generateJwt(user.id, user.email, user.role)
-    return res.json({token})
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
 
   async login(req, res, next) {
-    const {email, password} = req.body
-    const user = await User.findOne({where: {email}})
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
     if (!user) {
-        return next(ApiError.internal('User with that name not found'))
+      return next(ApiError.internal("User with that name not found"));
     }
 
-    let comparePassword = bcrypt.compareSync(password, user.password)
-    if(!comparePassword) {
-        return next(ApiError.internal('Incorrect password'))
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Incorrect password"));
     }
 
-    const token = generateJwt(user.id, user.email, user.role)
-    return res.json({token})
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
 
   async check(req, res, next) {
-
-    const token = generateJwt(req.user.id, req.user.email, req.user.role)
-    return res.json({token})
-      
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
 
     // res.json('kjgfdnhjgnhdfjug')
     // const { id } = req.query;
@@ -59,15 +55,25 @@ class userController {
 
     // res.json(id);
 
-
-
     // If we type in router http://localhost:5000/api/user/auth?id=5&messege=kdsngjnd (messega after ?) that give us json file in curret page
   }
 
   async getUsers(req, res) {
-    const users = await User.findAll()
-    return res.json(users)
-}
+    const users = await User.findAll();
+    return res.json(users);
+  }
+
+  async changeRole(req, res, next) {
+    try {
+      const { email, role } = req.body;
+      const users = await User.findOne({ where: {email} });
+      users.set({ role: role });
+      await users.save();
+      return res.json(users);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
 }
 
 module.exports = new userController();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,13 +2,13 @@ const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
-
+const checkRole = require('../middleware/checkRoleMiddleware')
 
 router.post('/registration', userController.registration)
 router.post('/login', userController.login)
 router.get('/auth',authMiddleware, userController.check)
-router.get('/users', userController.getUsers)
-
+router.get('/users', checkRole('ADMIN'), userController.getUsers)
+router.post('/users', checkRole('ADMIN'), userController.changeRole)
 
 
 // router.get('/auth', (req, res) => {
