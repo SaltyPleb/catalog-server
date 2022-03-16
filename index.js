6a91ccc98972fd9b93179584af422e36634406e8
SaltyPleb <sergun4ik22@gmail.com>
2022-03-16 23:55:51 +0300

    last last try to deploy

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var http = require('http');
 app.use(express.json())
 // app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
+app.set('port', PORT);
 app.use('/api', router)
 var server = http.createServer(app);
 
@@ -28,6 +29,37 @@ app.get('/', (req, res) => {
 app.use(errorHandler)
 
 
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+}
+
 // app.get('/', (req, res) => {
   //     res.status(200).json({message: 'WORKING'})
   // })
@@ -38,6 +70,8 @@ app.use(errorHandler)
       await sequelize.authenticate()
       await sequelize.sync()
       server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+      server.on('error', onError);
+      server.on('listening', onListening);
     // app.listen(PORT, () => console.log(`listening on port ${PORT}`))
   } catch (e) {
     console.log(e);
