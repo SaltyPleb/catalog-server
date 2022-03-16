56578806608c4e712f09c9632c1b5827192971c3
SaltyPleb <sergun4ik22@gmail.com>
2022-03-16 23:48:52 +0300

    last try

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
+var http = require('http');
 app.use(express.json())
 // app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
+var server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.end('<h1>WHY THIS THIS NOT WORKING</h1>')
@@ -27,15 +29,16 @@ app.use(errorHandler)
 
 
 // app.get('/', (req, res) => {
-//     res.status(200).json({message: 'WORKING'})
-// })
-
-
-const start = async () => {
-  try {
-    await sequelize.authenticate()
-    await sequelize.sync()
-    app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+  //     res.status(200).json({message: 'WORKING'})
+  // })
+  
+  
+  const start = async () => {
+    try {
+      await sequelize.authenticate()
+      await sequelize.sync()
+      server.listen(PORT, () => console.log(`listening on port ${PORT}`));
+    // app.listen(PORT, () => console.log(`listening on port ${PORT}`))
   } catch (e) {
     console.log(e);
   }
