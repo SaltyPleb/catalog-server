670edbd876465eacd2e3408c550cfb2e1ab9bfef
eugenebul <yauheni.bulyha@mapbox.com>
2022-04-03 12:57:16 +0300

    ex

--- utils/consts.js
+++ utils/consts.js
@@ -1,13 +1,46 @@
+const description_consts = require("./description.consts");
+
 module.exports = Object.freeze({
   TYPES_CONST: [
-    { type: "CPU", dataFile: require("./data/processors.json") },
-    { type: "GPU", dataFile: require("./data/gpu.json") },
-    { type: "Motherboard", dataFile: require("./data/motherboard.json") },
-    { type: "RAM", dataFile: require("./data/ram.json") },
-    { type: "Storage", dataFile: require("./data/storage.json") },
-    { type: "Cooling(CPU)", dataFile: require("./data/cooling_cpu.json") },
-    { type: "Power supply", dataFile: require("./data/power_supply.json") },
-    { type: "Case", dataFile: require("./data/case.json") },
+    {
+      type: "CPU",
+      dataFile: require("./data/processors.json"),
+      description: description_consts.CPU_DES,
+    },
+    {
+      type: "GPU",
+      dataFile: require("./data/gpu.json"),
+      description: description_consts.GPU_DES,
+    },
+    {
+      type: "Motherboard",
+      dataFile: require("./data/motherboard.json"),
+      description: description_consts.MOTHERBOARD_DES,
+    },
+    {
+      type: "RAM",
+      dataFile: require("./data/ram.json"),
+      description: description_consts.RAM_DES,
+    },
+    {
+      type: "Storage",
+      dataFile: require("./data/storage.json"),
+      description: description_consts.STORAGE_DES,
+    },
+    {
+      type: "Cooling(CPU)",
+      dataFile: require("./data/cooling_cpu.json"),
+      description: description_consts.COOLING_CPU_DES,
+    },
+    {
+      type: "Power supply",
+      dataFile: require("./data/power_supply.json"),
+      description: description_consts.POWER_SUPPLY_DES,
+    },
+    {
+      type: "Case",
+      dataFile: require("./data/case.json"),
+      description: description_consts.CASE_DES,
+    },
   ],
-  BRANDS_CONST: [],
 });
--- utils/tests.js
+++ utils/tests.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const ApiError = require("../error/ApiError");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const {
@@ -11,13 +10,9 @@ const {
   Type,
   Brand,
 } = require("../models/models");
-const fs = require("fs");
-
-const searchClient = require("./imageSearch");
-const cpuFill = require("./executors/cpuFill");
-const gpuFill = require("./executors/gpuFill");
 const typeFill = require("./executors/typesFill");
 const brandFill = require("./executors/brandsFill");
+const devicesFill = require("./executors/devicesFill");
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
@@ -43,22 +38,15 @@ const test = async () => {
     const basket = await Basket.create({ userId: user.id });
     const token = await generateJwt(user.id, user.email, user.role);
 
-    console.log(token);
-
-    let cpuData = require("./data/processors.json");
-    let gpuData = require("./data/gpu.json");
+    console.log(`new user token: ${token}`);
 
     typeFill();
 
     brandFill();
+    
+    // deviceFill has limit params, thats indicate how many devices will be created for each type
+    devicesFill(2);
 
-    // await Promise.all(cpuData.slice(0, 10).map((row, index) => {
-    //   cpuFill(row, index, cpuData)
-    // }));
-
-    // await Promise.all(gpuData.slice(0, 10).map((row, index) => {
-    //   gpuFill(row, index, gpuData)
-    // }));
   } catch (err) {
     console.error(err);
   }
