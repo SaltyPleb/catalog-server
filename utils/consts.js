d9a6d36d550d94bb1d2417b723ffcb12b9e6c5e6
eugenebul <yauheni.bulyha@mapbox.com>
2022-04-03 11:13:15 +0300

    ex

--- utils/consts.js
+++ utils/consts.js
@@ -1,13 +1,13 @@
 module.exports = Object.freeze({
   TYPES_CONST: [
-    { type: "CPU2", dataFile: require("./data/processors.json") },
-    { type: "GPU2", dataFile: require("./data/gpu.json") },
-    { type: "Motherboard", dataFile: "" },
-    // { type: "RAM", dataFile: "" },
-    // { type: "Storage", dataFile: "" },
-    // { type: "Cooling(CPU)", dataFile: "" },
-    // { type: "Power supply", dataFile: "" },
-    // { type: "Case", dataFile: "" },
+    { type: "CPU", dataFile: require("./data/processors.json") },
+    { type: "GPU", dataFile: require("./data/gpu.json") },
+    { type: "Motherboard", dataFile: require("./data/motherboard.json") },
+    { type: "RAM", dataFile: require("./data/ram.json") },
+    { type: "Storage", dataFile: require("./data/storage.json") },
+    { type: "Cooling(CPU)", dataFile: require("./data/cooling_cpu.json") },
+    { type: "Power supply", dataFile: require("./data/power_supply.json") },
+    { type: "Case", dataFile: require("./data/case.json") },
   ],
   BRANDS_CONST: [],
 });
--- utils/tests.js
+++ utils/tests.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const ApiError = require("../error/ApiError");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
@@ -14,6 +16,8 @@ const fs = require("fs");
 const searchClient = require("./imageSearch");
 const cpuFill = require("./executors/cpuFill");
 const gpuFill = require("./executors/gpuFill");
+const typeFill = require("./executors/typesFill");
+const brandFill = require("./executors/brandsFill");
 
 const generateJwt = (id, email, role) => {
   return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
@@ -38,24 +42,23 @@ const test = async () => {
     );
     const basket = await Basket.create({ userId: user.id });
     const token = await generateJwt(user.id, user.email, user.role);
-    
+
     console.log(token);
 
     let cpuData = require("./data/processors.json");
-    let gpuData = require("./data/gpu.json")
+    let gpuData = require("./data/gpu.json");
 
-    const type = await Type.bulkCreate([{ name: "CPU" }], {
-      ignoreDuplicates: true,
-    });
+    typeFill();
 
-    await Promise.all(cpuData.slice(0, 10).map((row, index) => {
-      cpuFill(row, index, cpuData)
-    }));
+    brandFill();
 
-    await Promise.all(gpuData.slice(0, 10).map((row, index) => {
-      gpuFill(row, index, gpuData)
-    }));
-    
+    // await Promise.all(cpuData.slice(0, 10).map((row, index) => {
+    //   cpuFill(row, index, cpuData)
+    // }));
+
+    // await Promise.all(gpuData.slice(0, 10).map((row, index) => {
+    //   gpuFill(row, index, gpuData)
+    // }));
   } catch (err) {
     console.error(err);
   }
